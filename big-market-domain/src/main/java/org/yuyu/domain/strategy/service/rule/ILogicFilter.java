91cd90dd21b16ace160b0ae010e201f114f2c483
Bingo5422 <2269797530@qq.com>
2026-08-21 11:38:44 +0800

    feat：实现抽奖前置规则规则过滤【黑名单、权重】

---
+++ big-market-domain/src/main/java/org/yuyu/domain/strategy/service/rule/ILogicFilter.java
@@ -0,0 +1,12 @@
+package org.yuyu.domain.strategy.service.rule;
+
+
+import org.yuyu.domain.strategy.model.entity.RuleActionEntity;
+import org.yuyu.domain.strategy.model.entity.RuleMatterEntity;
+
+/**
+ * 抽奖规则过滤接口
+ */
+public interface ILogicFilter<T extends RuleActionEntity.RaffleEntity> {
+    RuleActionEntity<T> filter(RuleMatterEntity ruleMatterEntity);
+}
